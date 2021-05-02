d2dc151fcb91cd69305ca3b78d8ce5ad86d54db6
Rafael Santos <raffrenan@gmail.com>
2021-05-02 10:13:47 -0300

    Refatorando e otimizando código, deletando controllers que não foram usados.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\Auth\LoginController;
 use App\Http\Controllers\User\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Tips\TipController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,12 +28,12 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('user.auth.logou
 Route::get('/register', [RegisterController::class, 'register'])->name('user.auth.register');
 Route::post('/registerDo', [RegisterController::class, 'registerDo'])->name('user.auth.registerDo');
 
-// Logged
+// Tips
 Route::group(['middleware' => ['auth']], function() {
-    Route::match(['post', 'get'],'/tips', [UserController::class, 'tips'])->name('user.tips');
-    Route::post('/tips/search', [UserController::class, 'tipsSearch'])->name('user.tips.search');
-    Route::get('/tips/my', [UserController::class, 'myTips'])->name('user.tips.my');
-    Route::get('/tips/create', [UserController::class, 'tipsCreate'])->name('user.tips.create');
-    Route::post('/tips/createDo', [UserController::class, 'tipsCreateDo'])->name('user.tips.tipsCreateDo');
-    Route::post('/tips/del', [UserController::class, 'tipsDel'])->name('user.tips.tipsDel');
+    Route::match(['post', 'get'],'/tips', [TipController::class, 'tips'])->name('user.tips');
+    Route::post('/tips/search', [TipController::class, 'tipsSearch'])->name('user.tips.search');
+    Route::get('/tips/my', [TipController::class, 'myTips'])->name('user.tips.my');
+    Route::get('/tips/create', [TipController::class, 'tipsCreate'])->name('user.tips.create');
+    Route::post('/tips/createDo', [TipController::class, 'tipsCreateDo'])->name('user.tips.tipsCreateDo');
+    Route::post('/tips/del', [TipController::class, 'tipsDel'])->name('user.tips.tipsDel');
 });
